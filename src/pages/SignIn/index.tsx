582b6dfe68f118369bdea3b2dc3d0a9c088edd61
LeonardoCHb <leonardohably@hotmail.com>
2020-09-15 19:07:48 -0300

    basic form input/button bug

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,7 +9,9 @@ import {
     from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
+
 import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -26,7 +28,7 @@ import {
 import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
-    const formRef
+    const formRef= useRef<FormHandles>(null)
     const navigation = useNavigation()
 
     const handleSignIn = useCallback((data: object) => {
@@ -51,12 +53,14 @@ const SignIn: React.FC = () => {
                 <Title>Faça seu logon</Title>
                 </View>
 
-                <Form onSubmit={handleSignIn}>
+                <Form ref={formRef} onSubmit={handleSignIn}>
                 <Input name="email" icon="mail" placeholder="E-mail"/>
 
                 <Input name="password" icon="lock" placeholder="Senha"/>
 
-                <Button onPress={() => {console.log('DEU')}}>Entrar</Button>
+                <Button onPress={() => {
+                    formRef.current?.submitForm()
+                }}>Entrar</Button>
 
                 <ForgotPassword onPress={() => {}}>
                     <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { 
     Image, 
     View, 
@@ -9,6 +9,9 @@ import {
     from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation} from '@react-navigation/native'
+
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 
 import Input from '../../components/Input'
@@ -24,6 +27,7 @@ import {
 import logoImg from '../../assets/logo.png'
 
 const SignUp: React.FC = () => {
+    const formRef= useRef<FormHandles>(null)
     const navigation = useNavigation()
 
     return (
@@ -44,12 +48,13 @@ const SignUp: React.FC = () => {
                 <Title>Crie sua conta</Title>
                 </View>
 
+                <Form ref={formRef} onSubmit={() => {}}>
                 <Input name="name" icon="user" placeholder="Nome"/>
                 
                 <Input name="email" icon="mail" placeholder="E-mail"/>
 
                 <Input name="password" icon="lock" placeholder="Senha"/>
-
+                </Form>
                 <Button onPress={() => {console.log('DEU')}}>Entrar</Button>
 
             </Container>
